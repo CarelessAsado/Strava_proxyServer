8b4206564651e3741ce841a2e0c094784ee46916
Rodrigo López <rodrigohernanlopez89@gmail.com>
2022-03-08 21:15:05 -0300

    needle POST route

--- back/server.js
+++ back/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { currentUrl } = require("./models/url");
 
+/*---------------------------*/
+const app = express();
+port = process.env.PORT || 5000;
+/*-------------------------------*/
 app.use(
   cors({
     origin: [currentUrl],
   })
 );
+/*---------------START DB and SERVER-------------------*/
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Atlas connected");
+  app.listen(port, () =>
+    console.log("Server connected at http://localhost:" + port)
+  );
+});
+const tokenRoute = require("./routes/tokens");
+app.use("/", tokenRoute);
